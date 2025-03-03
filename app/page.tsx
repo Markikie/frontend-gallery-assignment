4b4d16817f4270abaab85143674bdeb6daebfb37
ferin <sudawan91@gmail.com>
2025-03-03 09:46:32 +0700

    create header

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,10 @@ import Gallery from "../components/Gallery";
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-20">
+      <h1 className="text-3xl font-bold underline">
+        Image Gallery 
+        </h1>
       <Gallery />
     </main>
   );
